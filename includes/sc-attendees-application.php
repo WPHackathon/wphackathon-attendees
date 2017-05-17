8eb86d5e8e46f33f5477802fdb948078596495de
maugelves <mgelves@gmail.com>
2017-05-17 18:31:07 +0200

    Added a javascript file to verify the attendees registration form.

--- includes/sc-attendees-application.php
+++ includes/sc-attendees-application.php
@@ -3,150 +3,158 @@
 // Shortcode to show the form for Attendees Application
 function wphackathon_sc_attendees_application( $atts ) {
 
-  // Add Shortcode Attributes
-  $a = shortcode_atts( array(
+	// Enqueue Javascript file to validate form
+	wp_enqueue_script('attendees-application-js', plugins_url() . "/wphackathon-attendees/assets/js/attendees-application.js", array('jquery'), true);
 
-    'skills' => array("Font-end", "Back-end", "Designer", "Copywriter"),
+	// Enqueue Style
+	wp_enqueue_style('attendees-application-css', plugins_url() . "/wphackathon-attendees/assets/css/style.css", array(), false);
 
-  ), $atts );
+	// Add Shortcode Attributes
+	$a = shortcode_atts( array(
 
+		'skills' => array("Font-end", "Back-end", "Designer", "Copywriter"),
 
-  if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) ) {
-
-  	// Asign the form content to variables
-  	$name         = $_POST['wph-attendee-name'];
-  	$description  = $_POST['wph-attendee-description'];
-  	$email        = $_POST['wph-attendee-email'];
-    $twitter      = $_POST['wph-attendee-twitter'];
-    $orguser      = $_POST['wph-attendee-org-user'];
-    $explanation  = $_POST['wph-attendee-explanation'];
-    $organization = $_POST['wph-attendee-organization-selection'];
-    $skill        = $_POST['cat'];
-
-  	// Add the content of the form to $post as an array
-  	$post = array(
-  		'post_title'	  => $name,
-  		'post_content'	=> $description,
-      'post_category' => $skill,
-  		'post_status'	  => 'draft',
-  		'post_type'	    => 'attendee',
-      'meta_input'    => array(
-        'attendee_email'        => $email,
-        'attendee_twitter'      => $twitter,
-        'attendee_orguser'      => $orguser,
-        'attendee_explanation'  => $explanation,
-        'attendee_organization' => $organization,
-      ),
-  	);
-
-  	$post_id = wp_insert_post( $post );
-
-    wp_set_post_terms( $post_id, $_POST['cat'], 'skill', false );
-
-  } // end IF
-
-  ?>
+	), $atts );
 
 
-  <!-- New Attendee Form -->
+	if( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) ) {
 
-  <div id="postbox">
+		// Asign the form content to variables
+		$name         = $_POST['wph-attendee-name'];
+		$description  = $_POST['wph-attendee-description'];
+		$email        = $_POST['wph-attendee-email'];
+		$twitter      = $_POST['wph-attendee-twitter'];
+		$orguser      = $_POST['wph-attendee-org-user'];
+		$explanation  = $_POST['wph-attendee-explanation'];
+		$organization = $_POST['wph-attendee-organization-selection'];
+		$skill        = $_POST['cat'];
 
-    <form id="new_post" name="new_post" method="post" action="">
+		// Add the content of the form to $post as an array
+		$post = array(
+			'post_title'	  => $name,
+			'post_content'	=> $description,
+			'post_category' => $skill,
+			'post_status'	  => 'draft',
+			'post_type'	    => 'attendee',
+			'meta_input'    => array(
+				'attendee_email'        => $email,
+				'attendee_twitter'      => $twitter,
+				'attendee_orguser'      => $orguser,
+				'attendee_explanation'  => $explanation,
+				'attendee_organization' => $organization,
+			),
+		);
 
-      <p>
-        <label for="wph-attendee-name"><?php _e( 'First and Second Name (required)', $wph_textdomain ); ?></label><br />
-        <input type="text" id="wph-attendee-name" value="" name="wph-attendee-name" tabindex="2" size="20" / required>
-      </p>
+		$post_id = wp_insert_post( $post );
 
-      <p>
-        <label for="wph-attendee-email"><?php _e( 'Email - we use Gravatar service for the photograph (required)', $wph_textdomain); ?></label><br />
-        <input type="email" id="wph-attendee-email" name="wph-attendee-email" value="" tabindex="3" size="20" required>
-      </p>
+		wp_set_post_terms( $post_id, $_POST['cat'], 'skill', false );
 
-      <p>
-        <label for="wph-attendee-description"><?php _e( 'Talk us about: what do you do and what is your experience? (required)', $wph_textdomain ); ?></label><br />
-        <textarea id="wph-attendee-description" name="wph-attendee-description" tabindex="4" cols="50" rows="6" required></textarea>
-      </p>
+	} // end IF
 
-      <p>
-        <label for="wph-attendee-twitter"><?php _e( 'Twitter Username', $wph_textdomain); ?></label><br />
-        <input type="text" id="wph-attendee-twitter" name="wph-attendee-twitter" value="" tabindex="5" size="20">
-      </p>
+	?>
 
-      <p>
-        <label for="wph-attendee-org-user"><?php _e( 'WordPress.org Username', $wph_textdomain); ?></label><br />
-        <input type="text" id="wph-attendee-org-user" name="wph-attendee-org-user" value="" tabindex="6" size="20">
-      </p>
 
-      <p>
-        <label><?php _e( 'Select your Skill (required)', $wph_textdomain ); ?></label><br />
-          <?php
+    <!-- New Attendee Form -->
 
-          $select_cats = wp_dropdown_categories( array( 'echo' => 0, 'taxonomy' => 'skill', 'hide_empty' => 0 ) );
-          $select_cats = str_replace( "name='cat' id=", "name='cat[]' id=", $select_cats );
-          echo $select_cats;
+    <div id="postbox">
 
-          ?>
+        <form id="new_post" name="new_post" method="post" action="">
 
-      </p>
+            <p>
+                <label for="wph-attendee-name"><?php _e( 'First and Second Name (required)', $wph_textdomain ); ?></label><br />
+                <input type="text" id="wph-attendee-name" value="" name="wph-attendee-name" tabindex="2" size="20">
+            </p>
 
-      <?php
+            <p>
+                <label for="wph-attendee-email"><?php _e( 'Email - we use Gravatar service for the photograph (required)', $wph_textdomain); ?></label><br />
+                <input type="email" id="wph-attendee-email" name="wph-attendee-email" value="" tabindex="3" size="20">
+            </p>
 
-        // Add Organizations Query arguments
-        $args = array(
-          'order'          => 'asc',
-          'orderby'        => 'title',
-          'posts_per_page' => 30,
-          'post_type'      => 'organization'
-        );
+            <p>
+                <label for="wph-attendee-description"><?php _e( 'Talk us about: what do you do and what is your experience? (required)', $wph_textdomain ); ?></label><br />
+                <textarea id="wph-attendee-description" name="wph-attendee-description" tabindex="4" cols="50" rows="6"></textarea>
+            </p>
 
-        $the_query = new WP_Query( $args ); ?>
+            <p>
+                <label for="wph-attendee-twitter"><?php _e( 'Twitter Username', $wph_textdomain); ?></label><br />
+                <input type="text" id="wph-attendee-twitter" name="wph-attendee-twitter" value="" tabindex="5" size="20">
+            </p>
 
-        <?php if ( $the_query->have_posts() ) : ?>
+            <p>
+                <label for="wph-attendee-org-user"><?php _e( 'WordPress.org Username', $wph_textdomain); ?></label><br />
+                <input type="text" id="wph-attendee-org-user" name="wph-attendee-org-user" value="" tabindex="6" size="20">
+            </p>
 
-          <p>
-            <label><?php _e( 'With which Organization in particular would you like to take part?', $wph_textdomain ); ?></label><br />
+            <p>
+                <label><?php _e( 'Select your Skill (required)', $wph_textdomain ); ?></label><br />
+				<?php
 
-            <select class="" name="wph-attendee-organization-selection" tabindex="8">
+				$select_cats = wp_dropdown_categories( array( 'echo' => 0, 'taxonomy' => 'skill', 'hide_empty' => 0 ) );
+				$select_cats = str_replace( "name='cat' id=", "name='cat[]' id=", $select_cats );
+				echo $select_cats;
 
-              <option value="<?php _e( 'Anyone', $wph_textdomain ); ?>"><?php _e( 'Anyone', $wph_textdomain ); ?></option>
+				?>
 
-              <!-- the loop -->
-                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            </p>
 
-                  <option value="<?php the_title(); ?>"><?php the_title(); ?></option>
+			<?php
 
-                <?php endwhile; ?>
-              <!-- end of the loop -->
+			// Add Organizations Query arguments
+			$args = array(
+				'order'          => 'asc',
+				'orderby'        => 'title',
+				'posts_per_page' => 30,
+				'post_type'      => 'organization'
+			);
 
-            </select>
+			$the_query = new WP_Query( $args ); ?>
 
-          </p>
+			<?php if ( $the_query->have_posts() ) : ?>
 
-          <?php wp_reset_postdata(); ?>
+                <p>
+                    <label><?php _e( 'With which Organization in particular would you like to take part?', $wph_textdomain ); ?></label><br />
 
-        <?php else : ?>
+                    <select class="" name="wph-attendee-organization-selection" tabindex="8">
 
-        <?php endif; ?>
-      </p>
+                        <option value="<?php _e( 'Anyone', $wph_textdomain ); ?>"><?php _e( 'Anyone', $wph_textdomain ); ?></option>
 
-      <p>
-        <label for="wph-attendee-explanation"><?php _e( 'Why do you want to participate in WPHackathon? (required)', $wph_textdomain ); ?></label><br />
-        <textarea id="wph-attendee-explanation" name="wph-attendee-explanation" tabindex="9" cols="50" rows="6" required></textarea>
-      </p>
+                        <!-- the loop -->
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-      <p><input type="submit" value="<?php _e( 'Participate', $wph_textdomain ); ?>" tabindex="10" id="submit" name="submit" /></p>
+                            <option value="<?php the_title(); ?>"><?php the_title(); ?></option>
 
-      <input type="hidden" name="action" value="new_attendee" />
+						<?php endwhile; ?>
+                        <!-- end of the loop -->
 
-    </form>
+                    </select>
 
-  </div>
+                </p>
 
-  <!--// New Attendee Form -->
+				<?php wp_reset_postdata(); ?>
 
-  <?php
+			<?php else : ?>
+
+			<?php endif; ?>
+            </p>
+
+            <p>
+                <label for="wph-attendee-explanation"><?php _e( 'Why do you want to participate in WPHackathon? (required)', $wph_textdomain ); ?></label><br />
+                <textarea id="wph-attendee-explanation" name="wph-attendee-explanation" tabindex="9" cols="50" rows="6"></textarea>
+            </p>
+
+            <div class="alert alert-danger hidden" role="alert"><?php _e('Some fields are required', $wph_textdomain); ?></div>
+
+            <p><input type="submit" value="<?php _e( 'Participate', $wph_textdomain ); ?>" tabindex="10" id="submit" name="submit" /></p>
+
+            <input type="hidden" name="action" value="new_attendee" />
+
+        </form>
+
+    </div>
+
+    <!--// New Attendee Form -->
+
+	<?php
 
 }
 add_shortcode( 'wph_attendees_application', 'wphackathon_sc_attendees_application' );

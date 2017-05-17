1120d7f9f6a23da1f82ea2b12f1fd86d4375a0a6
Rafa Poveda <raven@raven.es>
2017-05-18 00:33:14 +0200

    typo fix

--- includes/create-custom-pages.php
+++ includes/create-custom-pages.php
@@ -23,7 +23,7 @@ function wphackathon_create_pages($network_wide) {
 function wphackathon_create_pages_new_site($blog_id, $user_id, $domain, $path, $site_id, $meta) {
 
   //replace with your base plugin path E.g. dirname/filename.php
-  if ( is_plugin_active_for_network( 'wphackathon-attendees/wphackathon-attendees.php' ) {
+  if ( is_plugin_active_for_network( 'wphackathon-attendees/wphackathon-attendees.php' ) ) {
     switch_to_blog($blog_id);
     	wphackathon_custom_pages();
     restore_current_blog();
